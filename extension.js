fbb4f7548054c0a239af16ae4a856d9e2cc56af5
kenitek <adam.p.kwong@gmail.com>
2014-09-30 03:00:36 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -99,6 +99,19 @@
             }
         },
         
+        bot.commands.riggedCommand = {
+            command: 'rigged',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Roulette not even rigged :opieop:");
+                }
+            }
+        },
+        
         bot.commands.hpCommand = {
             command: 'hp',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
@@ -295,7 +308,7 @@
         afkRankCheck: "ambassador",
         motdEnabled: true,
         motdInterval: 10,
-        motd: "New :kappa: Bot Commands: !fatdarude, !feg, !flyin, !hp, !kolento, !lol, !nipaface, !notgay, !notsanic, !rucka, !sanic, !scotland, !wtf",
+        motd: "New :kappa: Bot Commands: !fatdarude, !feg, !flyin, !hp, !kolento, !lol, !nipaface, !notgay, !notsanic, !rigged, !rucka, !sanic, !scotland, !wtf",
         filterChat: false,
         etaRestriction: false,
         welcome: false,
