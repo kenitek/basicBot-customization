55036f4509fa9ac9bfe6f323ef38cdc1dfbb5cd4
kenitek <adam.p.kwong@gmail.com>
2014-09-30 10:26:13 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -125,6 +125,19 @@
             }
         },
         
+        bot.commands.krCommand = {
+            command: 'kr',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me http://i.imgur.com/x4YcuTx.jpg");
+                }
+            }
+        },
+        
         bot.commands.sanicCommand = {
             command: 'sanic',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
@@ -347,7 +360,7 @@
         afkRankCheck: "ambassador",
         motdEnabled: true,
         motdInterval: 10,
-        motd: "New :kappa: Bot Commands: !clap, !fatdarude, !feg, !flyin, !hp, !kolento, !lol, !nipaface, !notgay, !notsanic, !rigged, !rucka, !sanic, !scotland, !win, !wtf",
+        motd: "New :kappa: Bot Commands: !clap, !fatdarude, !feg, !flyin, !hp, !kolento, !kr, !lol, !nipaface, !notgay, !notsanic, !rigged, !rucka, !sanic, !scotland, !win, !wtf.",
         filterChat: false,
         etaRestriction: false,
         welcome: false,
