0da1c9e84fc261ac1ec14077e2b31c6090a2be64
kenitek <adam.p.kwong@gmail.com>
2014-09-30 01:15:41 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -44,7 +44,7 @@
                 }
             }
         },
-        
+            
         bot.commands.slapCommand = {
                 command: 'slap',
                 rank: 'user',
@@ -110,7 +110,20 @@
                     API.sendChat("/me http://i.imgur.com/S5UL4NB.jpg");
                 }
             }
-        },  
+        },
+        
+        bot.commands.ruckaCommand = {
+            command: 'rucka',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me rucka rucka :dansgame:");
+                }
+            }
+        },
             
         bot.commands.kolentoCommand = {
             command: 'kolento',  //The command to be called. With the standard command literal this would be: !bacon
