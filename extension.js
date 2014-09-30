2c7d6a06e8ea445b07432c43c0207fe9b9235348
kenitek <adam.p.kwong@gmail.com>
2014-09-30 15:28:11 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -412,7 +412,7 @@
         afkRankCheck: "ambassador",
         motdEnabled: true,
         motdInterval: 10,
-        motd: "New :kappa: Bot Commands: !bait, !clap, !fatdarude, !feg, !flyin, !hp, !kev, !kolento, !kr, !lol, !nipaface, !notgay, !notsanic, !rekt, !rigged, !rucka, !sanic, !scotland, !tup, !victorfm, !win, !wtf.",
+        motd: "Bot Commands: http://pastebin.com/U4hS11n3 Rules: http://pastebin.com/6NEPtZW9 Look for here if you're new to Plug.dj: http://i.imgur.com/WfBlklF.jpg or here: http://pastebin.com/a4eqQJ79",
         filterChat: false,
         etaRestriction: false,
         welcome: false,
