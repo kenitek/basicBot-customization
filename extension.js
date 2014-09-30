d2076c5e903ff2ef5b7b0fd62b5bb383e1facad1
kenitek <adam.p.kwong@gmail.com>
2014-09-30 10:35:47 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -44,6 +44,19 @@
                 }
             }
         },
+        
+        bot.commands.rektCommand = {
+            command: 'rekt',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me NOT REKT ☐ REKT ☑   ");
+                }
+            }
+        },   
             
         bot.commands.slapCommand = {
                 command: 'slap',
