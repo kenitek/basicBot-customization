b596a3d394ea297ba1d71ba836fece62889eeec0
kenitek <adam.p.kwong@gmail.com>
2014-09-30 01:42:17 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -99,6 +99,19 @@
             }
         },
         
+        bot.commands.hpCommand = {
+            command: 'hp',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me heil Hornpub :forsensheffy:7:forsensheffy:/");
+                }
+            }
+        },
+        
         bot.commands.sanicCommand = {
             command: 'sanic',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
