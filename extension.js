c79607d854d4fcca8da35d2464b2bb78ebd522b9
kenitek <adam.p.kwong@gmail.com>
2014-10-02 18:50:29 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -58,6 +58,32 @@
             }
         },
         
+        bot.commands.jepCommand = {
+            command: 'jep',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me http://i.imgur.com/MTmwzqE.png");
+                }
+            }
+        },
+        
+        bot.commands.dealCommand = {
+            command: 'deal',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me http://i.imgur.com/GCn5BGT.gif");
+                }
+            }
+        },
+        
         bot.commands.victorfm = {
             command: 'victorfm',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
