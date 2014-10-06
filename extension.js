4ec63e0204725ed3d63c02c81e1f67ad77ce02ae
kenitek <adam.p.kwong@gmail.com>
2014-10-06 16:15:10 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -292,15 +292,15 @@
             }
         },
         
-        bot.commands.yoshiiCommand = {
-            command: 'yoshii',  //The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.chatripCommand = {
+            command: 'chatrip',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me http://i.imgur.com/RlJUTnH.png");
+                    API.sendChat("/me RIP in peace Chat :kappa:");
                 }
             }
         },
