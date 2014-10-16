e3d6c878ca8aa8e6fe89f732f7873d6c905974ae
kenitek <adam.p.kwong@gmail.com>
2014-10-16 00:17:58 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -58,6 +58,19 @@
             }
         },
         
+        bot.commands.fingCommand = {
+            command: 'fing',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me http://i.imgur.com/phr9V7v.png");
+                }
+            }
+        },
+        
         bot.commands.ohmuCommand = {
             command: 'ohmu',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
