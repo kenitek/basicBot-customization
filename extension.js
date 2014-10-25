d8f1adda483d7b41e51c4c1e5b366ab11940b85e
kenitek <adam.p.kwong@gmail.com>
2014-10-25 15:45:24 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -58,6 +58,19 @@
             }
         },
         
+        bot.commands.bannerCommand = {
+            command: 'banner',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me http://imgur.com/a/bYuHO");
+                }
+            }
+        },
+        
         bot.commands.tpCommand = {
             command: 'tp',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
