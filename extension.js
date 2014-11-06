2dc1607ffbcb288dad1bc948b474c4eb7cff66ed
kenitek <adam.p.kwong@gmail.com>
2014-11-05 22:40:43 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -45,6 +45,20 @@
             }
         },
         
+        bot.commands.newfegCommand = {
+            command: 'newfeg',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me https://www.youtube.com/watch?v=pXIoB9RPl6w");
+                }
+            }
+        },
+        
+        
         bot.commands.snoopCommand = {
             command: 'snoop',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
