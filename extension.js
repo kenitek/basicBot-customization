b09b33ef5ef056ffa62935523ffb298ab1357245
kenitek <adam.p.kwong@gmail.com>
2014-11-13 23:06:24 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -411,7 +411,7 @@
             }
         },
         
-        +        bot.commands.gravCommand = {
+        bot.commands.gravCommand = {
             command: 'grav',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
