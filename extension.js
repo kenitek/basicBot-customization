bdeceb3d271aabc31bc85f3dae10db3c7d3723bd
kenitek <adam.p.kwong@gmail.com>
2014-11-20 19:17:25 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -56,8 +56,8 @@
             }
         },
         
-                                bot.commands.BOSSCommand = {
-            command: 'keni',  //The command to be called. With the standard command literal this would be: !bacon
+                                bot.commands.bossCommand = {
+            command: 'boss',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
