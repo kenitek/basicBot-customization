8de02042c2a83d385d91dc7838db2a0a26ff02a9
kenitek <adam.p.kwong@gmail.com>
2014-11-25 12:36:35 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -28,7 +28,7 @@
          }
          }
          }
-         */
+         */(◕‿◕✿) Yuki Yuki (◕‿◕✿)
 
                 bot.commands.baconCommand = {
             command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
@@ -39,6 +39,19 @@
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
                     API.sendChat("/me Bacon!!!");
+                }
+            }
+        },
+        
+         bot.commands.yukiCommand = {
+            command: 'yuki',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me (◕‿◕✿) YUKI YUKI (◕‿◕✿)"
                 }
             }
         },
