89426d10d8fa3a8dfb451687d5a60d8ffbedf957
kenitek <adam.p.kwong@gmail.com>
2014-11-24 21:54:55 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -43,6 +43,19 @@
             }
         },
         
+                        bot.commands.greenshotCommand = {
+            command: 'greenshot',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Stop Using Gyazo and Start Using Greenshot :swiftrage::swiftrage::swiftrage: Start the Revolution http://getgreenshot.org/");
+                }
+            }
+        },
+        
                         bot.commands.snusCommand = {
             command: 'snus',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
