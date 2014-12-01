604c2a82cb4d30397d3764c693132583c0108b63
kenitek <adam.p.kwong@gmail.com>
2014-12-01 15:34:54 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1058,6 +1058,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://raw.githubusercontent.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
 
 }).call(this);
