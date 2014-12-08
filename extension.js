a75d458cb005cac1fbd4efd7e58609fe409985c5
kenitek <adam.p.kwong@gmail.com>
2014-12-08 15:22:31 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1075,7 +1075,7 @@
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         maximumAfk: 5000,
         afkRemoval: false,
-        maximumDc: 25,
+        maximumDc: 30,
         bouncerPlus: false,
         lockdownEnabled: false,
         lockGuard: false,
