8dbb7b5e44e55cec0e2f15009318b493ef443f07
kenitek <adam.p.kwong@gmail.com>
2014-12-08 08:21:37 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -54,6 +54,18 @@
                 }
             }
         },
+                                bot.commands.commandsCommand = {
+            command: 'commands',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me http://pastebin.com/U4hS11n3");
+                }
+            }
+        },
         
                                 bot.commands.krippCommand = {
             command: 'kripp',  //The command to be called. With the standard command literal this would be: !bacon
